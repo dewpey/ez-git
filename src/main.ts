d795ad92e8073fe1c7f3f1fff76cfe44dcc204ba
Drew Patel <drew1patel@gmail.com>
2020-11-30 22:31:50 -0600

    Changes

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,78 @@
-/**
- * Some predefined delays (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+import { exit } from "process";
+const shell = require('shelljs');
+
+const synonyms = {
+  update: 'update',
+  commit: 'update',
+  finish: 'update',
+  save: 'update',
+  undo: 'undo',
+  revert: 'undo',
+  reset: 'undo',
+  oops: 'undo',
+  refresh: 'refresh',
+  pull: 'refresh',
+  fetch: 'refresh',
 }
 
-/**
- * Returns a Promise<string> that resolves after given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
+if (!shell.which('git')) {
+  shell.echo('Sorry, this script requires git');
+  exit(1);
 }
 
-// Below are examples of using ESLint errors suppression
-// Here it is suppressing missing return type definitions for greeter function
+const command = synonyms[process.argv[2]]
+const arg = process.argv[3]
+if(command == null){
+  shell.echo("Error: You forgot an action or action not found")
+  exit(1)
+} 
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
+
+switch (command) {
+  case 'update':
+    if(command == null){
+      shell.echo("Error: You forgot an action")
+      exit(1)
+    } 
+    shell.echo("update")
+    shell.exec("git add .")
+    shell.exec("git stage -a")
+    shell.exec(`git commit -m ${arg}`)
+    shell.exec("git push")
+
+    shell.echo("First we added all the files in the current directory with: git add .")
+    shell.echo("Then we stage all the changes with: git stage -a")
+    shell.echo("Then we commit the changes with your message with: git commit -m")
+    shell.echo("Finally, push all the changes up with: git push")
+
+    break
+  case 'undo':
+    if(command == null) shell.exec("git reset HEAD~ --soft")
+    else shell.exec(`git reset HEAD~${command} --soft`)
+
+    shell.echo("We just need to reset from the current head softly with: git reset HEAD~(amount) --soft")
+
+    break
+  case 'refresh':
+    shell.exec('git pull')
+    break
+  case 'fix':
+    shell.exec('git fetch origin')
+    shell.exec('git checkout master')
+    shell.exec('git reset --hard origin/master')
+    shell.exec('git clean -d --force')
+
+    shell.echo("We just need to get the latest changes from the repo: git fetch origin")
+    shell.echo("Then we grab the master branch: git checkout master")
+    shell.echo("Then we reset our local repo: git reset --hard origin/master")
+    shell.exec("Then we forcefully clean up and changes we made: git clean -d --force")
+
+    break
+  default:
+    shell.echo("No command found")
+    exit(1)
 }
+
+
+
+shell.exit(0)
